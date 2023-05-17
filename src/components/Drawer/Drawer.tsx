81235e5bd988137fa560317bccecb606d47c99fa
Omar Monteon <monteon.omar@gmail.com>
2023-05-17 11:46:50 -0600

    feat: implemented Drawer component
    feat: used Drawer component for main menu in mobile
    
    refactor: updated how Button and Drawer components are imported

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,5 +1,54 @@
-function Drawer() {
-  return <div>Drawer</div>;
+import { ReactNode, useEffect, useRef } from "react";
+import FocusTrap from "focus-trap-react";
+import styles from "./drawer.module.css";
+
+interface DrawerProps {
+  isOpen: boolean;
+  toggleButton?: ReactNode;
+  toggle: () => void;
+  children?: ReactNode;
+}
+
+function Drawer({
+  isOpen = false,
+  toggleButton,
+  toggle,
+  children,
+}: DrawerProps) {
+  // Handle 'Escape' key press event to close the drawer
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.code === "Escape" && isOpen) {
+        toggle();
+      }
+    };
+    document.addEventListener("keydown", handleEscape);
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [isOpen, toggle]);
+
+  return (
+    <FocusTrap paused={!isOpen}>
+      <div>
+        {toggleButton}
+        <div className={`${styles.overlay} ${isOpen ? styles.open : ""}`}></div>
+        <aside
+          id="drawer"
+          className={`${styles.drawer} ${isOpen ? styles.open : ""}`}
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="drawer-title"
+        >
+          <h2 id="drawer-title" tabIndex={-1} className="sr-only">
+            Menu
+          </h2>
+
+          {children}
+        </aside>
+      </div>
+    </FocusTrap>
+  );
 }
 
 export default Drawer;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,8 @@ import { ReactComponent as CloseIcon } from "../../assets/close-icon.svg";
 import { ReactComponent as HamIcon } from "../../assets/ham-icon.svg";
 import { ReactComponent as ChevronIcon } from "../../assets/chevron-icon.svg";
 import { useState } from "react";
-import Button from "../../components/Button/Button";
+import Button from "../../components/Button";
+import Drawer from "../../components/Drawer";
 
 function HomePage() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -19,15 +20,32 @@ function HomePage() {
           <h1>Frontend Mentor</h1>
           <h2>Feedback board</h2>
         </div>
-        {menuOpen ? (
-          <CloseIcon onClick={toggleMenu} />
-        ) : (
-          <HamIcon onClick={toggleMenu} />
-        )}
+        <Drawer
+          isOpen={menuOpen}
+          toggleButton={
+            <Button
+              type="clean"
+              ariaExpanded={menuOpen}
+              ariaControls="drawer"
+              onClick={toggleMenu}
+            >
+              {menuOpen ? <CloseIcon /> : <HamIcon />}
+            </Button>
+          }
+          toggle={toggleMenu}
+        >
+          <Button
+            tabIndex={0}
+            onClick={() => console.log("Click inside drawer")}
+          >
+            button
+          </Button>
+        </Drawer>
       </header>
       <main className={styles.main}>
         <header>
           <p>
+            {/* TODO: Create and use dropdown component here */}
             Sort by : Most Upvotes <ChevronIcon />{" "}
           </p>
           <Button onClick={() => console.log("Add Feedback")}>
