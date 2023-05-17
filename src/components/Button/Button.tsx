1c0573ae7039258d31df08bb08b6214a0cba3070
Omar Monteon <monteon.omar@gmail.com>
2023-05-17 09:03:02 -0600

    feat: new button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,19 @@
-function Button() {
-  return <div>Button</div>;
+import { ReactNode } from "react";
+import styles from "./button.module.css";
+
+type ButtonType = "primaryPurple" | "primaryBlue" | "dark" | "danger";
+
+interface MyProps {
+  type?: ButtonType;
+  onClick?: () => void;
+  children?: ReactNode;
+}
+
+function Button({ type = "primaryPurple", children, onClick }: MyProps) {
+  return (
+    <button className={`${styles.button} ${styles[type]}`} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 export default Button;
