5cc4d38865111aa1d9794c5c1bac100da837b577
Omar Monteon <monteon.omar@gmail.com>
2023-05-18 15:05:47 -0600

    feat: created Sidebar component
    refactor: moved the drawer in home inside Sidebar
    
    - And imported Sidebar in home instead

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { ReactComponent as CloseIcon } from "@assets/close-icon.svg";
+import { ReactComponent as HamIcon } from "@assets/ham-icon.svg";
+import Drawer from "@components/Drawer";
+import Button from "@components/Button";
+import Card from "@components/Card";
+import Tag from "@components/Tag";
+import styles from "./sidebar.module.css";
+
+const { CheckableTag } = Tag;
+
+interface SidebarProps {
+  open: boolean;
+  toggle: () => void;
+}
+
+// TODO: Get this data from API, localstorage or somewhere else
+const tags = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"];
+
+function Sidebar({ open = false, toggle }: SidebarProps) {
+  const [checkedTag, setCheckedTag] = useState<string>("");
+
+  const handleCheckTag = (tag: string) => {
+    setCheckedTag(tag);
+    // TODO: Update list of cards depending on tag
+    // TODO: Probably move this to the parent component
+  };
+
+  return (
+    <Drawer
+      isOpen={open}
+      toggleButton={
+        <Button
+          type="clean"
+          ariaExpanded={open}
+          ariaControls="drawer"
+          onClick={toggle}
+        >
+          {open ? <CloseIcon /> : <HamIcon />}
+        </Button>
+      }
+      toggle={toggle}
+    >
+      <Card className={styles.tagsCard}>
+        {/* TODO: This might be better to do as a list for a11y reasons. */}
+        {tags.map((tag) => (
+          <CheckableTag
+            checked={tag === checkedTag}
+            key={tag}
+            onClick={() => handleCheckTag(tag)}
+          >
+            {tag}
+          </CheckableTag>
+        ))}
+      </Card>
+    </Drawer>
+  );
+}
+
+export default Sidebar;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import { ReactComponent as CloseIcon } from "@assets/close-icon.svg";
-import { ReactComponent as HamIcon } from "@assets/ham-icon.svg";
 import { ReactComponent as ChevronIcon } from "@assets/chevron-icon.svg";
 import Button from "@components/Button";
-import Drawer from "@components/Drawer";
 import styles from "./home.module.css";
-import Card from "@components/Card";
+import Sidebar from "@components/Sidebar";
 
 // Next tasks
-// 1. Create Card component
+// 1. Create Card component [DONE]
 // 2. Create CheckableTag component
+// * I think checkable tag can exist by itself
+// * However, in this particular case, we need a list of those
+// that behave like a radio button instead
 // 3. Create Badge component
 // 4. Implement Tag filters card component
 // 5. Implement Roadmap card component
@@ -17,10 +17,10 @@ import Card from "@components/Card";
 // 7. Implement empty home card component
 
 function HomePage() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
   };
 
   return (
@@ -30,29 +30,7 @@ function HomePage() {
           <h1>Frontend Mentor</h1>
           <h2>Feedback board</h2>
         </div>
-        <Drawer
-          isOpen={menuOpen}
-          toggleButton={
-            <Button
-              type="clean"
-              ariaExpanded={menuOpen}
-              ariaControls="drawer"
-              onClick={toggleMenu}
-            >
-              {menuOpen ? <CloseIcon /> : <HamIcon />}
-            </Button>
-          }
-          toggle={toggleMenu}
-        >
-          <Card>
-            <Button
-              tabIndex={0}
-              onClick={() => console.log("Click inside drawer")}
-            >
-              button
-            </Button>
-          </Card>
-        </Drawer>
+        <Sidebar open={sidebarOpen} toggle={toggleSidebar} />
       </header>
       <main className={styles.main}>
         <header>
