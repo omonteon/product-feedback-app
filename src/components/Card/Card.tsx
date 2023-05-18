ae6651b08e64121b941870e283be723653cb0392
Omar Monteon <monteon.omar@gmail.com>
2023-05-18 10:56:25 -0600

    feat: create Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,12 @@
-function Card() {
-  return <div>Card</div>;
+import { ReactNode } from "react";
+import styles from "./card.module.css";
+
+interface CardProps {
+  children?: ReactNode;
+}
+
+function Card({ children }: CardProps) {
+  return <div className={styles.card}>{children}</div>;
 }
 
 export default Card;
