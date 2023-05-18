f39ea35f6f87be0b2c56265f1f15b2df01df48cf
Omar Monteon <monteon.omar@gmail.com>
2023-05-18 15:04:58 -0600

    feat: added support for className prop for Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react";
 import styles from "./card.module.css";
 
 interface CardProps {
+  className?: string;
   children?: ReactNode;
 }
 
-function Card({ children }: CardProps) {
-  return <div className={styles.card}>{children}</div>;
+function Card({ className, children }: CardProps) {
+  return <div className={`${styles.card} ${className}`}>{children}</div>;
 }
 
 export default Card;
