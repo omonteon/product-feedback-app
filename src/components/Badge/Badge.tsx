b88aeb7dc1345e713b514ba1ffa4fb6c2a22a3dc
Omar Monteon <monteon.omar@gmail.com>
2023-05-19 11:11:15 -0600

    feat: created Badge component

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,19 @@
-function Badge() {
-  return <div>Badge</div>;
+import { ReactNode } from "react";
+import styles from "./badge.module.css";
+
+interface BadgeProps {
+  color: string;
+  text: string | ReactNode;
+  className?: string;
+}
+
+function Badge({ color, text, className = "" }: BadgeProps) {
+  return (
+    <span className={`${styles.badge} ${className}`}>
+      <span className={styles.dot} style={{ backgroundColor: color }}></span>
+      <span>{text}</span>
+    </span>
+  );
 }
 
 export default Badge;
