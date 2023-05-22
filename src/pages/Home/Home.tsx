e001cb12b0bb6b4744c560da0a9e777e658878d5
Omar Monteon <monteon.omar@gmail.com>
2023-05-22 15:18:43 -0600

    feat: added a hardcoded list of feedback
    feat: implemented mapping of FeedbackCard elements
    
    style: added properties for list of feedback in the main content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,8 @@ import Button from "@components/Button";
 import styles from "./home.module.css";
 import Sidebar from "@components/Sidebar";
 import EmptyFeedback from "@components/EmptyFeedback";
+import { Feedback } from "src/interfaces/Feedback";
+import FeedbackCard from "@components/FeedbackCard";
 
 // Next tasks
 // 1. Create Card component [DONE]
@@ -11,11 +13,32 @@ import EmptyFeedback from "@components/EmptyFeedback";
 // * I think checkable tag can exist by itself
 // * However, in this particular case, we need a list of those
 // that behave like a radio button instead
-// 3. Create Badge component
-// 4. Implement Tag filters card component
-// 5. Implement Roadmap card component
-// 6. Implement DropDown component
-// 7. Implement empty home card component
+// 3. Create Badge component [DONE]
+// 4. Implement Tag filters card component [DONE]
+// 5. Implement Roadmap card component [DONE]
+// 6. Implement DropDown component [PENDING]
+// 7. Implement empty home card component [DONE]
+
+// TODO: Get this from some API or from localStorage
+const feedbackList: Array<Feedback> = [
+  {
+    id: 0,
+    title: "Add tags for solutions",
+    description: "Easier to search for solutions based on a specific stack.",
+    tag: "Enhancement",
+    upVoteCount: 112,
+    commentCount: 2,
+  },
+  {
+    id: 1,
+    title: "Add a dark theme option",
+    description:
+      "It would help people with light sensitivities and who prefer dark mode.",
+    tag: "Feature",
+    upVoteCount: 99,
+    commentCount: 4,
+  },
+];
 
 function HomePage() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -47,7 +70,13 @@ function HomePage() {
           </Button>
         </header>
         <section className={styles.mainContent}>
-          <EmptyFeedback />
+          {feedbackList.length === 0 ? (
+            <EmptyFeedback />
+          ) : (
+            feedbackList.map((feedback) => (
+              <FeedbackCard key={feedback.id} feedback={feedback} />
+            ))
+          )}
         </section>
       </main>
     </>
