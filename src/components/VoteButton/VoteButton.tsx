5e9922eb61780753e1538764e790b6d1a62f86fa
Omar Monteon <monteon.omar@gmail.com>
2023-05-23 09:20:13 -0600

    feat: VoteButton component (wip)

--- src/components/VoteButton/VoteButton.tsx
+++ src/components/VoteButton/VoteButton.tsx
@@ -1,5 +1,25 @@
-function VoteButton() {
-  return <div>VoteButton</div>;
+import { ReactComponent as ChevronIcon } from "@assets/chevron-icon.svg";
+import styles from "./voteButton.module.css";
+
+interface VoteButtonProps {
+  count?: number;
+  upVoted?: boolean;
+  onChange?: () => void;
+}
+
+function VoteButton({ count = 0, upVoted = false, onChange }: VoteButtonProps) {
+  return (
+    <label className={styles.voteButton}>
+      <ChevronIcon className={styles.upVoteIcon} />
+      <input
+        type="checkbox"
+        checked={upVoted}
+        onChange={onChange}
+        aria-checked={upVoted}
+      />
+      <b>{count}</b>
+    </label>
+  );
 }
 
 export default VoteButton;
