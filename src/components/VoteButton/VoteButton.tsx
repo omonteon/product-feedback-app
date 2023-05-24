f2bb1b17c4fda4c7c8237881139b120a2b4f1cf0
Omar Monteon <monteon.omar@gmail.com>
2023-05-24 09:41:15 -0600

    feat: added className prop to VoteButton
    style: completed VoteButton styling

--- src/components/VoteButton/VoteButton.tsx
+++ src/components/VoteButton/VoteButton.tsx
@@ -2,14 +2,20 @@ import { ReactComponent as ChevronIcon } from "@assets/chevron-icon.svg";
 import styles from "./voteButton.module.css";
 
 interface VoteButtonProps {
+  className?: string;
   count?: number;
   upVoted?: boolean;
   onChange?: () => void;
 }
 
-function VoteButton({ count = 0, upVoted = false, onChange }: VoteButtonProps) {
+function VoteButton({
+  className = "",
+  count = 0,
+  upVoted = false,
+  onChange,
+}: VoteButtonProps) {
   return (
-    <label className={styles.voteButton}>
+    <label className={`${styles.voteButton} ${className}`}>
       <ChevronIcon className={styles.upVoteIcon} />
       <input
         type="checkbox"
