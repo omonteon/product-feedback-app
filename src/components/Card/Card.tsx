f7c77d0f75476d62ef85b807190457063fbbacf7
Omar Monteon <monteon.omar@gmail.com>
2023-05-25 11:30:11 -0600

    feat: added link feature to card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from "react";
 import styles from "./card.module.css";
+import { Link } from "react-router-dom";
 
 interface CardProps {
   className?: string;
+  to?: string;
   children?: ReactNode;
 }
 
-function Card({ className, children }: CardProps) {
-  return <div className={`${styles.card} ${className}`}>{children}</div>;
+function Card({ className, to, children }: CardProps) {
+  const content = (
+    <div className={`${styles.card} ${className}`}>{children}</div>
+  );
+
+  return to ? (
+    <Link to={to} className={styles.linkWrapper}>
+      {content}
+    </Link>
+  ) : (
+    content
+  );
 }
 
 export default Card;
