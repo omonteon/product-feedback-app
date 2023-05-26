282a123eb010678e7b8d01d3d2621ce1ce552916
Omar Monteon <monteon.omar@gmail.com>
2023-05-26 11:02:40 -0600

    feat: updated feedbackAPI with json structure

--- src/api/FeedbackAPI.ts
+++ src/api/FeedbackAPI.ts
@@ -1,23 +1,52 @@
-import { Feedback } from "src/interfaces/Feedback";
+import {
+  Feedback,
+  FeedbackAPIResponse,
+  ProductRequest,
+} from "src/interfaces/Feedback";
 
 function getFeedbackList(): Feedback[] {
-  const feedbackListJSONStr = localStorage.getItem("feedbackList");
-  return JSON.parse(feedbackListJSONStr ?? "");
+  const dataStr: string = localStorage.getItem("data") ?? "";
+  const data: FeedbackAPIResponse = JSON.parse(dataStr ?? "");
+  const productRequests: ProductRequest[] = data.productRequests;
+
+  return adaptProductRequestsToFeedbackList(productRequests);
 }
 
 function getFeedbackById(id: number | undefined): Feedback {
   if (id === undefined) {
     throw new Error("There was no id provided to get the feedback item.");
   }
-  const feedbackListJSONStr: string =
-    localStorage.getItem("feedbackList") ?? "";
-  const feedbackList: Feedback[] = JSON.parse(feedbackListJSONStr ?? "");
-  const feedbackItem =
-    feedbackList.find((feedback) => feedback.id === id) ?? null;
-  if (feedbackItem === null) {
+  const dataStr: string = localStorage.getItem("data") ?? "";
+  const data: FeedbackAPIResponse = JSON.parse(dataStr ?? "");
+  const productRequests: ProductRequest[] = data.productRequests;
+  const productRequestItem = productRequests.find((pr) => pr.id === id) ?? null;
+  if (productRequestItem === null) {
     throw new Error(`Feedback item with id ${id} was not found`);
   }
-  return feedbackItem;
+  // TODO: Convert this in a different function
+  return adaptProductRequestToFeedback(productRequestItem);
 }
 
+function adaptProductRequestToFeedback(
+  productRequest: ProductRequest
+): Feedback {
+  return {
+    id: productRequest.id,
+    title: productRequest.title,
+    description: productRequest.description,
+    tag: productRequest.category,
+    status: productRequest.status,
+    upVoteCount: productRequest.upvotes,
+    upVoted: false,
+    commentCount: productRequest.comments?.length ?? 0,
+  };
+}
+
+function adaptProductRequestsToFeedbackList(
+  productRequests: ProductRequest[]
+): Feedback[] {
+  return productRequests.map((productRequest) => {
+    return adaptProductRequestToFeedback(productRequest);
+  });
+}
 export { getFeedbackList, getFeedbackById };
