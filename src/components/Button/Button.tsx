ca78562964cb173863da0dbb19920a5a7d25d72e
Omar Monteon <monteon.omar@gmail.com>
2023-05-26 11:01:57 -0600

    feat: added link wrapper for buttons
    * Using the "to" prop wraps the button in a link

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import styles from "./button.module.css";
+import { Link } from "react-router-dom";
 
 type ButtonType = "primaryPurple" | "primaryBlue" | "dark" | "danger" | "clean";
 
 interface ButtonProps {
   tabIndex?: number;
   type?: ButtonType;
+  to?: string;
   onClick?: () => void;
   children?: ReactNode;
   ariaExpanded?: boolean;
@@ -15,12 +17,13 @@ interface ButtonProps {
 function Button({
   tabIndex = 0,
   type = "primaryPurple",
+  to,
   children,
   ariaExpanded,
   ariaControls,
   onClick,
 }: ButtonProps) {
-  return (
+  const content = (
     <button
       aria-expanded={ariaExpanded}
       aria-controls={ariaControls}
@@ -30,6 +33,14 @@ function Button({
     >
       {children}
     </button>
+  );
+
+  return to ? (
+    <Link to={to} className={styles.linkWrapper}>
+      {content}
+    </Link>
+  ) : (
+    content
   );
 }
 export default Button;
