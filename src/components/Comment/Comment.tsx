1fb99e31420584ccfa4ab4e68aa8a37f7411065c
Omar Monteon <monteon.omar@gmail.com>
2023-05-27 16:25:13 -0600

    feat: created Comment component

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,5 +1,40 @@
-function Comment() {
-  return <div>Comment</div>;
+import { Comment } from "src/interfaces/Feedback";
+import AvatarTestImage from "@assets/avatar-test.png";
+import styles from "./comment.module.css";
+import Button from "@components/Button";
+import CommentReply from "./CommentReply";
+
+interface CommentProps {
+  className?: string;
+  comment: Comment;
+}
+
+function Comment({ className = "", comment }: CommentProps) {
+  const { user, content, replies = [] } = comment;
+  return (
+    <div className={`${styles.comment} ${className}`}>
+      <header>
+        <div className={styles.userInfo}>
+          <img src={AvatarTestImage} alt={user.name} />
+          <div>
+            <h5>{user.name}</h5>
+            <p>@{user.username}</p>
+          </div>
+        </div>
+        {/* <img src={user.image} /> */}
+        <Button type="link">Reply</Button>
+      </header>
+      <p>{content}</p>
+      {replies.length > 0 ? (
+        <div className={styles.replies}>
+          {replies.map((reply) => {
+            // TODO: Replies are missing an id
+            return <CommentReply key={reply.replyingTo} reply={reply} />;
+          })}
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default Comment;
