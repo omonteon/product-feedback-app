ddfa65b14dd09f5ba891887ca1aceab6ab24e07c
Omar Monteon <monteon.omar@gmail.com>
2023-06-05 09:07:19 -0600

    test: link wrapper button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,15 +1,31 @@
 import { test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { fireEvent, render } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import Button from "./Button";
 
-test("Button: displays a default button", () => {
-  render(
+test("Should display a default button", () => {
+  const app = render(
     <MemoryRouter>
       <Button>Click me</Button>
     </MemoryRouter>
   );
+  app.unmount();
+});
 
-  const button = screen.getByRole("button", { name: /click me/i });
-  expect(button).toBeDefined();
+test("Should execute handler on click", () => {
+  const app = render(
+    <MemoryRouter initialEntries={["/home"]}>
+      <Routes>
+        <Route path="home" element={<h1>Home</h1>} />
+        <Route path="about" element={<h1>About</h1>} />
+      </Routes>
+
+      <Button to="../about">Click me</Button>
+    </MemoryRouter>
+  );
+  const button = app.getByRole("link", { name: /click me/i });
+  expect(app.getByRole("heading", { name: /home/i })).toBeDefined();
+  fireEvent.click(button);
+  expect(app.getByRole("heading", { name: /about/i })).toBeDefined();
+  app.unmount();
 });
