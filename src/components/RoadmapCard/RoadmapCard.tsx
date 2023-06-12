69535a583afa0d63cc0bbca4042c0f3a287c57bf
Omar Monteon <monteon.omar@gmail.com>
2023-06-12 11:25:14 -0600

    feat: created roadmap card component
    feat: retrieved status data from api

---
+++ src/components/RoadmapCard/RoadmapCard.tsx
@@ -0,0 +1,72 @@
+import { Link, useAsyncValue } from "react-router-dom";
+import { Feedback } from "src/interfaces/Feedback";
+import Badge from "@components/Badge";
+import Card from "@components/Card";
+import styles from "./roadmapCard.module.css";
+
+function RoadmapCard() {
+  const [feedbackList] = useAsyncValue() as [Feedback[]];
+  const countByStatus = getCountbyStatus(feedbackList);
+  const statusList = [
+    {
+      text: "Planned",
+      count: countByStatus.planned,
+      color: getCSSVariableValue("--accent-orange"),
+    },
+    {
+      text: "In-Progress",
+      count: countByStatus["in-progress"],
+      color: getCSSVariableValue("--primary-purple"),
+    },
+    {
+      text: "Live",
+      count: countByStatus.live,
+      color: getCSSVariableValue("--accent-blue"),
+    },
+  ];
+
+  return (
+    <Card className={styles.roadMapCard}>
+      <header>
+        <h3>Roadmap</h3> <Link to="roadmap">View</Link>
+      </header>
+      <ul>
+        {statusList.map((status) => (
+          <li key={status.text}>
+            <Badge
+              color={status.color}
+              text={
+                <>
+                  {status.text} <b>{status.count}</b>
+                </>
+              }
+              className={styles.roadMapBadge}
+            />
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+// TODO: Probably move to utils
+function getCSSVariableValue(varName: string) {
+  const style = getComputedStyle(document.body);
+  return style.getPropertyValue(varName);
+}
+
+function getCountbyStatus(feedbackList: Feedback[]) {
+  const countByStatus = {
+    suggestion: 0,
+    planned: 0,
+    "in-progress": 0,
+    live: 0,
+  };
+  feedbackList.forEach((feedback) => {
+    countByStatus[feedback.status] += 1;
+  });
+
+  return countByStatus;
+}
+
+export default RoadmapCard;
