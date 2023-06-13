9ef2f50efa6fe9fc00807211ae576c13aa2396fe
Omar Monteon <monteon.omar@gmail.com>
2023-06-13 11:03:20 -0600

    feat: created a TagsCard component
    refactor: moved all tags card logic out of sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import {
-  useAsyncValue,
-  useLoaderData,
-  useNavigation,
-  useSubmit,
-} from "react-router-dom";
+import { useAsyncValue, useLoaderData } from "react-router-dom";
 import { ReactComponent as CloseIcon } from "@assets/close-icon.svg";
 import { ReactComponent as HamIcon } from "@assets/ham-icon.svg";
 import Drawer from "@components/Drawer";
 import Button from "@components/Button";
-import Card from "@components/Card";
-import Tag from "@components/Tag";
 import RoadmapSummaryCard from "@components/RoadmapSummaryCard";
-import styles from "./sidebar.module.css";
+import TagsCard from "@components/TagsCard";
 import { Feedback } from "src/interfaces/Feedback";
-
-const { CheckableTag } = Tag;
 
 interface SidebarProps {
   open: boolean;
@@ -25,22 +15,6 @@ interface SidebarProps {
 function Sidebar({ open = false, toggle }: SidebarProps) {
   const [feedbackList] = useAsyncValue() as [Feedback[]];
   const { q } = useLoaderData() as { q: string };
-  const navigation = useNavigation();
-  const defaultTag = q ? q : "All";
-  const [checkedTag, setCheckedTag] = useState<string>(defaultTag);
-  const submit = useSubmit();
-
-  const searching =
-    navigation.location &&
-    new URLSearchParams(navigation.location.search).has("q");
-
-  // TODO: Get this data from API, localstorage or somewhere else
-  const tags = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"];
-
-  const handleCheckTag = (tag: string) => {
-    setCheckedTag(tag);
-    submit({ q: tag });
-  };
 
   return (
     <Drawer
@@ -57,21 +31,7 @@ function Sidebar({ open = false, toggle }: SidebarProps) {
       }
       toggle={toggle}
     >
-      <Card
-        className={`${styles.tagsCard} ${searching ? styles.searching : ""}`}
-      >
-        {/* TODO: This might be better to do as a list for a11y reasons. */}
-        {tags.map((tag) => (
-          <CheckableTag
-            checked={tag === checkedTag}
-            key={tag}
-            disabled={searching}
-            onClick={() => handleCheckTag(tag)}
-          >
-            {tag}
-          </CheckableTag>
-        ))}
-      </Card>
+      <TagsCard defaultTag={q} />
       <RoadmapSummaryCard feedbackList={feedbackList} />
     </Drawer>
   );
---
+++ src/components/TagsCard/TagsCard.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useNavigation, useSubmit } from "react-router-dom";
+import Card from "@components/Card";
+import CheckableTag from "@components/Tag/CheckableTag";
+import styles from "./tagsCard.module.css";
+
+function TagsCard({ defaultTag = "All" }) {
+  const navigation = useNavigation();
+  const [checkedTag, setCheckedTag] = useState(defaultTag);
+  const submit = useSubmit();
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  // TODO: Get this data from API, localstorage or somewhere else
+  const tags = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"];
+
+  const handleCheckTag = (tag: string) => {
+    setCheckedTag(tag);
+    submit({ q: tag });
+  };
+
+  return (
+    <Card className={`${styles.tagsCard} ${searching ? styles.searching : ""}`}>
+      {/* TODO: This might be better to do as a list for a11y reasons. */}
+      {tags.map((tag) => (
+        <CheckableTag
+          checked={tag === checkedTag}
+          key={tag}
+          disabled={searching}
+          onClick={() => handleCheckTag(tag)}
+        >
+          {tag}
+        </CheckableTag>
+      ))}
+    </Card>
+  );
+}
+
+export default TagsCard;
