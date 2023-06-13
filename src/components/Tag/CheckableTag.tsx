3d06bc12032eca024f474bbafeb1d3a15aa0c84d
Omar Monteon <monteon.omar@gmail.com>
2023-06-13 10:50:19 -0600

    feat: added disabled property to checkable tags

--- src/components/Tag/CheckableTag.tsx
+++ src/components/Tag/CheckableTag.tsx
@@ -3,18 +3,21 @@ import styles from "./tag.module.css";
 
 interface CheckableTagProps {
   checked?: boolean;
+  disabled?: boolean;
   children?: ReactNode;
-  onClick?: () => void;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 function CheckableTag({
   checked = false,
+  disabled = false,
   children,
   onClick,
 }: CheckableTagProps) {
   return (
     <button
       className={`${styles.tag} ${checked ? styles.checked : ""}`}
+      disabled={disabled}
       onClick={onClick}
     >
       {children}
