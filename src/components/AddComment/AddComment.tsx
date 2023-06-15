1c4e4f47b16cf9bccd719a21d88fc28a3c7ecdbc
Omar Monteon <monteon.omar@gmail.com>
2023-06-14 21:06:16 -0600

    feat: implement fetcher to add comment to the API

--- src/components/AddComment/AddComment.tsx
+++ src/components/AddComment/AddComment.tsx
@@ -2,8 +2,10 @@ import Card from "@components/Card";
 import styles from "./addComment.module.css";
 import Button from "@components/Button";
 import { ChangeEvent, useState } from "react";
+import { Form, useFetcher } from "react-router-dom";
 
 interface AddCommentProps {
+  feedbackId: string;
   className?: string;
 }
 
@@ -14,11 +16,16 @@ const MAX_CHARS = 250;
 // 2. Add form validation
 // 3. Add form interaction
 
-function AddComment({ className = "" }: AddCommentProps) {
+function AddComment({ feedbackId, className = "" }: AddCommentProps) {
+  const [comment, setComment] = useState("");
   const [charsLeft, setCharsLeft] = useState(MAX_CHARS);
+  const fetcher = useFetcher();
 
   const handleTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const textLength = event.target.value.length;
+    if (MAX_CHARS - textLength >= 0) {
+      setComment(event.target.value);
+    }
     setCharsLeft(MAX_CHARS - textLength);
   };
 
@@ -26,22 +33,32 @@ function AddComment({ className = "" }: AddCommentProps) {
     <Card className={`${styles.container} ${className || ""}`}>
       <h3>Add Comment</h3>
 
-      <form className={styles.form}>
+      <Form className={styles.form}>
         <textarea
           placeholder="Type your comment here"
           onChange={handleTextAreaChange}
+          value={comment}
         ></textarea>
         <footer>
           <p>{charsLeft <= 0 ? 0 : charsLeft} characters left</p>
           <Button
             htmlType="submit"
             type="primaryPurple"
-            disabled={charsLeft < 0}
+            onClick={() => {
+              fetcher.submit(
+                {
+                  comment: comment.toString(),
+                  feedbackId: feedbackId.toString(),
+                  intent: "addComment",
+                },
+                { method: "put" }
+              );
+            }}
           >
             Post Comment
           </Button>
         </footer>
-      </form>
+      </Form>
     </Card>
   );
 }
