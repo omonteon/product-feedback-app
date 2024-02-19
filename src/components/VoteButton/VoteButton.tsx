5a3c2a840e22c410b20fdc7007cb8079fe3e4eee
Omar Monteon <monteon.omar@gmail.com>
2024-02-18 21:37:21 -0600

    fix: votebutton was triggering redirect
    - In order to fix this, I added a wrapper that stops propagation of the click event
    - Recommendation about this was found here: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/0d5321a5457c5f0da0ca216053cc5b4f571b53ae/docs/rules/no-noninteractive-element-interactions.md

--- src/components/VoteButton/VoteButton.tsx
+++ src/components/VoteButton/VoteButton.tsx
@@ -25,34 +25,33 @@ function VoteButton({
     voteCount = formUpvotes;
   }
 
-  // FIXME: When you click/tap the padding of the label, it does not trigger the checkbox
-
   return (
-    <label className={`${styles.voteButton} ${className}`}>
-      <ChevronIcon className={styles.upVoteIcon} />
-      <input
-        name="upvotes"
-        type="checkbox"
-        checked={checked}
-        value={voteCount}
-        onClick={(event) => event.stopPropagation()}
-        onChange={() => {
-          const upvotes = upVoted ? count - 1 : count + 1;
-          // For some reason RR6 requires all form data to be strings
-          fetcher.submit(
-            {
-              upVoted: (upvotes > count).toString(),
-              upvotes: upvotes.toString(),
-              feedbackId: feedbackId.toString(),
-              intent: "upVote",
-            },
-            { method: "put" }
-          );
-        }}
-        aria-checked={checked}
-      />
-      <b>{voteCount}</b>
-    </label>
+    <div onClick={(event) => event.stopPropagation()} role="presentation">
+      <label className={`${styles.voteButton} ${className}`}>
+        <ChevronIcon className={styles.upVoteIcon} />
+        <input
+          name="upvotes"
+          type="checkbox"
+          checked={checked}
+          value={voteCount}
+          onChange={() => {
+            const upvotes = upVoted ? count - 1 : count + 1;
+            // For some reason RR6 requires all form data to be strings
+            fetcher.submit(
+              {
+                upVoted: (upvotes > count).toString(),
+                upvotes: upvotes.toString(),
+                feedbackId: feedbackId.toString(),
+                intent: "upVote",
+              },
+              { method: "put" }
+            );
+          }}
+          aria-checked={checked}
+        />
+        <b>{voteCount}</b>
+      </label>
+    </div>
   );
 }
 
