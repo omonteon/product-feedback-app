eae7be07a796345032b22d2135d8b3b711f1a64b
Omar Monteon <monteon.omar@gmail.com>
2024-03-10 18:10:35 -0600

    refactor: removed inlined components from Home
    - removed styles that are used in those components.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,22 @@
 import { Suspense, useState } from "react";
 import {
   Await,
-  useAsyncValue,
   useLoaderData,
   useNavigation,
   useSearchParams,
-  useSubmit,
 } from "react-router-dom";
-import { CurrentUser, Feedback, Vote } from "src/interfaces/Feedback";
+import { CurrentUser, Feedback } from "src/interfaces/Feedback";
 import { ReactComponent as ChevronIcon } from "@assets/chevron-icon.svg";
-import { ReactComponent as BulbIcon } from "@assets/bulb-icon.svg";
-import Select from "react-dropdown-select";
 import Button from "@components/Button";
 import Sidebar from "@components/Sidebar";
-import EmptyFeedback from "@components/EmptyFeedback";
-import FeedbackCard from "@components/FeedbackCard";
 import Skeleton from "@components/Skeleton";
 import Card from "@components/Card";
 import styles from "./home.module.css";
 import TagsCard from "@components/TagsCard";
 import RoadmapSummaryCard from "@components/RoadmapSummaryCard";
+import FeedbackList from "@components/FeedbackList";
 
 // Next tasks
-// 0. Add "status" field when editing feedback. Filter suggestions in home to show only "suggestions" (4 pomodoros) [DONE in ~1 pomodoro]
-// 1. Implement filtering by tag (3 pomodoros) [Done in ~3 pomodoros]
-// 2. Show correct number of feedback by category in the sidebar (2 pomodoros) [DONE in 1 pomodoro]
-// 3. Implement comment creation and replies too (4 pomodoros) [Done 6 pomodoros]
-// 4. Implement sorting dropdown (2 pomodoros) [Done 2 pomodoros]
-// 5. Implement sorting logic in the "API" (2 pomodoros) [Done 2 pomodoros]
 // 6. Create RoadmapCard component (1 pomodoro)
 // 7. Create Column component (1 pomodoro)
 // 8. Create Tabs component (3 pomodoros)
@@ -52,6 +41,7 @@ import RoadmapSummaryCard from "@components/RoadmapSummaryCard";
 // i. Create custom hooks
 // j. Make a schema for the forms (https://www.taniarascia.com/schema-based-form-system/)
 
+// TODO: This is repeated in several places, put it in a shared file
 type HomeDataTuple = [Feedback[], CurrentUser];
 type HomeData = {
   data: HomeDataTuple;
@@ -144,89 +134,13 @@ function HomePage() {
               </div>
             </header>
             <main className={styles.main}>
-              <FeedbackListHeader loading={loading} />
-              <FeedbackList />
+              <FeedbackList loading={loading} />
             </main>
           </div>
         </Await>
       </Suspense>
     </>
   );
-}
-
-// TODO: Take this component out of this file
-function FeedbackListHeader({ loading }: { loading: boolean }) {
-  const submit = useSubmit();
-  const [feedbackList] = useAsyncValue() as HomeDataTuple;
-  const [searchParams] = useSearchParams();
-  const defaultSortingOption =
-    sortByOptions.find(
-      (option) => option.value === searchParams.get("sortBy")
-    ) ?? sortByOptions[0];
-
-  return (
-    <header>
-      <div>
-        <h3 className={styles.suggestionCount}>
-          {" "}
-          <BulbIcon /> {feedbackList?.length ?? 0} suggestions
-        </h3>
-        <Select
-          className={styles.select}
-          searchable={false}
-          options={sortByOptions}
-          values={[defaultSortingOption]}
-          contentRenderer={({ state }) => (
-            <div style={{ cursor: "pointer" }}>
-              Sort by : <b>{state.values[0].label}</b>
-            </div>
-          )}
-          dropdownHandleRenderer={({ state }) => (
-            <span
-              className={`${styles.selectHandle} ${
-                state.dropdown ? styles.active : ""
-              }`}
-            >
-              <ChevronIcon />
-            </span>
-          )}
-          disabled={loading}
-          onChange={(values) => {
-            const searchParamsObj = Object.fromEntries(searchParams);
-            submit({ ...searchParamsObj, sortBy: values[0].value });
-          }}
-          required
-        />
-      </div>
-      <Button to="/feedback/new">+ Add Feedback</Button>
-    </header>
-  );
-}
-// TODO: Take this component out of this file
-function FeedbackList() {
-  const [feedbackList, currentUser] = useAsyncValue() as HomeDataTuple;
-
-  return (
-    <section className={styles.mainContent}>
-      {feedbackList?.length === 0 ? (
-        <EmptyFeedback />
-      ) : (
-        feedbackList.map((feedback) => (
-          <FeedbackCard
-            key={feedback.id}
-            feedback={feedback}
-            redirectTo={`feedback/${feedback.id}`}
-            upVoted={isFeedbackUpVoted(currentUser.votes ?? [], feedback.id)}
-          />
-        ))
-      )}
-    </section>
-  );
-}
-
-// TODO: Move this into a utils module maybe?
-function isFeedbackUpVoted(userVotes: Vote[], feedbackId: string): boolean {
-  return userVotes.some((vote) => vote.productRequestId === feedbackId);
 }
 
 export default HomePage;
