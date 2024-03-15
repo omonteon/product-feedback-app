594dd605da39aaf9ead43d12202570216c49c1f8
Omar Monteon <monteon.omar@gmail.com>
2024-03-14 21:03:24 -0600

    feat: use roadmap header in the roadmap page component

--- src/pages/Roadmap/Roadmap.tsx
+++ src/pages/Roadmap/Roadmap.tsx
@@ -1,10 +1,10 @@
+import RoadmapHeader from "@components/RoadmapHeader";
 import styles from "./roadmap.module.css";
 
 function RoadmapPage() {
   return (
     <div className={`${styles.container}`}>
-      {/* <header className={styles.header}>
-  </header> */}
+      <RoadmapHeader />
       <main className={styles.main}>{/* <FeedbackListHeader /> */}</main>
     </div>
   );
