f0bc9eb7d20891c1e2894d872ad47544a0a41eca
Omar Monteon <monteon.omar@gmail.com>
2024-03-26 08:22:27 -0600

    chore: add default values while this is in wip

--- src/pages/Roadmap/Roadmap.tsx
+++ src/pages/Roadmap/Roadmap.tsx
@@ -1,39 +1,68 @@
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import RoadmapHeader from "@components/RoadmapHeader";
 import styles from "./roadmap.module.css";
+import { CurrentUser, Feedback } from "src/interfaces/Feedback";
+import { Await, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import FeedbackList from "@components/FeedbackList";
+
+type HomeDataTuple = [Feedback[], CurrentUser];
+type HomeData = {
+  data: HomeDataTuple;
+};
 
 function RoadmapPage() {
+  const { data } = useLoaderData() as HomeData;
+
+  console.log(data);
+
+  const tabs = [
+    {
+      title: "Planned",
+      description: "Ideas prioritized for research",
+      count: 2,
+    },
+    {
+      title: "In-Progress",
+      description: "Currently being developed",
+      count: 3,
+    },
+    { title: "Live", description: "Released features", count: 1 },
+  ];
+
   return (
-    <div className={`${styles.container}`}>
-      <RoadmapHeader />
-      <main className={styles.main}>
-        <Tabs selectedTabClassName={styles.selectedTab}>
-          <TabList className={styles.tabList}>
-            <Tab>Planned (2)</Tab>
-            <Tab>In-Progress (3)</Tab>
-            <Tab>Live (1)</Tab>
-          </TabList>
-          <TabPanel>
-            <header>
-              <h2>Planned (2)</h2>
-              <p>Ideas prioritized for research</p>
-            </header>
-          </TabPanel>
-          <TabPanel>
-            <header>
-              <h2>In-Progress (3)</h2>
-              <p>Currently being developed</p>
-            </header>
-          </TabPanel>
-          <TabPanel>
-            <header>
-              <h2>Live (1)</h2>
-              <p>Released features</p>
-            </header>
-          </TabPanel>
-        </Tabs>
-      </main>
-    </div>
+    <Suspense fallback={<>Fallback</>}>
+      <Await resolve={data} errorElement={<p>Error loading home data</p>}>
+        <div className={`${styles.container}`}>
+          <RoadmapHeader />
+          <main className={styles.main}>
+            <Tabs
+              selectedTabClassName={styles.selectedTab}
+              selectedTabPanelClassName={styles.selectedPanel}
+            >
+              <TabList className={styles.tabList}>
+                {tabs.map((tab) => (
+                  <Tab key={tab.title}>
+                    {tab.title} ({tab.count})
+                  </Tab>
+                ))}
+              </TabList>
+              {tabs.map((tab) => (
+                <TabPanel key={tab.title} className={styles.tabPanel}>
+                  <header>
+                    <h2>
+                      {tab.title} ({tab.count})
+                    </h2>
+                    <p>{tab.description}</p>
+                  </header>
+                  <FeedbackList />
+                </TabPanel>
+              ))}
+            </Tabs>
+          </main>
+        </div>
+      </Await>
+    </Suspense>
   );
 }
 
